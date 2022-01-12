cb45dfd1353616b4d36a3ff2917a8469f0c9d573
Franco Gallo <gallofrancogabriel@gmail.com>
2022-01-12 17:22:43 -0300

    Stock en cards

--- project-react-franco-gallo/src/components/ItemListContainer/Items/Item.js
+++ project-react-franco-gallo/src/components/ItemListContainer/Items/Item.js
@@ -4,14 +4,18 @@ import { Button } from 'react-bootstrap' // Esta es la forma de importar un comp
 import './Item.css'
 
 const Item = ({ item }) => {
+    let initial = 0
+
+    item.stock >= 1 ? initial=1 : initial=0
+    
     return (
         <section className='Item list-group list-group-horizontal'>
             <div className='list-group-item wholeItem'>
                 <h4>{item.name}</h4>
                 <img src={item.img} alt={item.name}/>
                 <Button variant="outline-dark">Más sobre el producto</Button>
-                <h6>Precio: $U {item.price}</h6>
-                <ItemCount initial={1} stock={item.stock}/>
+                <h6>Precio: $U {item.price}</h6> <h6>Stock: {item.stock}</h6>
+                <ItemCount initial={initial} stock={item.stock}/>
             </div>
         </section>
     )
